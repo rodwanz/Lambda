9cea56e431077062aba273c1708bdfddd1d5796f
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-12-28 07:47:05 -0300

    Declared lambda expression

--- src/application4/ProgramaPredicate.java
+++ src/application4/ProgramaPredicate.java
@@ -3,9 +3,9 @@ package application4;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities4.Product;
-import util.ProductPredicate;
 
 public class ProgramaPredicate {
 
@@ -19,7 +19,9 @@ public class ProgramaPredicate {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate);//METHOD REFERENCE
+		Predicate<Product> pred = p -> p.getPrice() >= 100;
+		
+		list.removeIf(pred);//METHOD REFERENCE
 		
 		for (Product p : list) {
 			System.out.println(p);
